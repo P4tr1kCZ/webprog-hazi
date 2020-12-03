e293c976a2be7f8e2ae2d9f9d565af8b73d1c1bc
P4tr1kCZ <patrik19990724@gmail.com>
2020-12-03 23:19:48 +0100

    Added basic views with localization support.

---
+++ view/layouts/language_select.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<ul id="languagechooser">
+    <li><a href="index.php?controller=language&amp;action=change&amp;lang=en">
+            <?= i18n("English") ?>
+        </a></li>
+    <li><a href="index.php?controller=language&amp;action=change&amp;lang=hu">
+            <?= i18n("Magyar") ?>
+        </a></li>
+</ul>
