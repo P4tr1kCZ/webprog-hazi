36cfae821de59dd3357035fdf46def2a9f1729f4
P4tr1kCZ <patrik19990724@gmail.com>
2020-12-03 22:58:12 +0100

    Added CommentsController

---
+++ controller/CommentsController.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once(__DIR__ . "/ControllerBase.php");
+require_once(__DIR__ . "/../db/DbPosts.php");
+require_once(__DIR__ . "/../db/DbComments.php");
+
+require_once(__DIR__ . "/../model/User.php");
+require_once(__DIR__ . "/../model/Post.php");
+require_once(__DIR__ . "/../model/Comment.php");
+
+class CommentsController extends ControllerBase
+{
+    private $dbComments;
+    private $dbPosts;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->dbComments = new DbComments();
+        $this->dbPosts = new DbPosts();
+    }
+
+    public function add()
+    {
+        if (!isset($this->currentUser)) {
+            throw new Exception("Not in session. Adding posts requires login");
+        }
+
+        if (isset($_POST["id"])) {
+
+            $postid = $_POST["id"];
+            $post = $this->dbPosts->findById($postid);
+
+            if ($post == NULL) {
+                throw new Exception("no such post with id: " . $postid);
+            }
+
+            $comment = new Comment();
+            $comment->setContent($_POST["content"]);
+            $comment->setAuthor($this->currentUser);
+            $comment->setPost($post);
+
+            try {
+                $comment->validateBeforeCreate();
+                $this->dbComments->insert($comment);
+                $this->view->setFlash("Comment \"" . $post->getTitle() . "\" successfully added.");
+                $this->view->redirect("posts", "view", "id=" . $post->getId());
+            } catch (ValidationException $ex) {
+                $errors = $ex->getErrors();
+                $this->view->setVariable("comment", $comment, true);
+                $this->view->setVariable("errors", $errors, true);
+                $this->view->redirect("posts", "view", "id=" . $post->getId());
+            }
+        } else {
+            throw new Exception("No such post id");
+        }
+    }
+}
